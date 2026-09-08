6b8eb7f353e34f64bbeb0ec9404851e80c01d26e
Meher Virk <Mehervirk@gmail.com>
2026-09-08 12:44:21 -0700

    Use 201 Content Created

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -18,10 +18,14 @@ function MyApp() {
 
     function updateList(person) { 
     postUser(person)
-      .then(() => setCharacters([...characters, person]))
-      .catch((error) => {
-        console.log(error);
+      .then((res) => {
+        if (res.status === 201) {
+            setCharacters([...characters, person]);
+        }
       })
+        .catch((error) => {
+        console.log(error);
+      });
     }
 
     function fetchUsers() {
