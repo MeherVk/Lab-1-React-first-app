1d3e0a79accb9105b18091e45a0db4332afe1f3d
Meher Virk <Mehervirk@gmail.com>
2026-09-08 18:33:30 -0700

    Return created object w/ id from POST

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -20,7 +20,10 @@ function MyApp() {
     postUser(person)
       .then((res) => {
         if (res.status === 201) {
-            setCharacters([...characters, person]);
+            res.json().then((json) => {
+                setCharacters([...characters, json]);
+            }
+            )
         }
       })
         .catch((error) => {
